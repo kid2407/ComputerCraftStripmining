c5e1db845b4612a5bc273d21f143ad52445fd9f7
Tobias Franz <cinex.mail@web.de>
2021-02-15 10:25:02 +0100

    Initial commit

---
+++ main.lua
@@ -0,0 +1,148 @@
+local function command_help()
+    print("This program support the following commands:")
+    print("- help <subcommand>: Get more information on a subcommand")
+    print("- tunnel: Dig a tunnel with the specified parameters")
+    print("- stripmine: Do some stripmining with the specified parameters")
+end
+
+print("Welcome to kid2407s mining program version 1.0.0!")
+command_help()
+
+local function isInteger(str)
+    return not (str == "" or str:find("%D")) -- str:match("%D") also works
+end
+
+local function calculate_required_fuel_for_mining(length, width, height, doStripmining)
+    local fuelUsage = 0
+
+    -- One vertical layer
+    fuelUsage = fuelUsage + ((width - 1) * (height - 1))
+    -- If height not even, then one more movement of width-1
+    if height % 2 == 1 then
+        fuelUsage = fuelUsage + width - 1
+    end
+
+    fuelUsage = fuelUsage + 1
+    fuelUsage = fuelUsage * length
+
+    if doStripmining then
+        local numberOfBranches = math.floor(length / 3)
+        fuelUsage = fuelUsage + (numberOfBranches * (2 * (9 + width)))
+    end
+
+    return fuelUsage
+end
+
+local function clear_terminal()
+    term.clear()
+    term.setCursorPos(1, 1)
+end
+
+local function do_mining(boolean, doStripmining)
+    clear_terminal()
+    local input = nil
+    local length = 3
+    local width = 1
+    local height = 2
+
+    while true do
+        print("Please enter a tunnel length:")
+        input = io.read()
+        if isInteger(input) then
+            length = tonumber(input)
+            break
+        end
+    end
+
+    while true do
+        print("Please enter a tunnel height:")
+        input = io.read()
+        if isInteger(input) then
+            height = tonumber(input)
+            break
+        end
+    end
+
+    while true do
+        print("Please enter a tunnel width:")
+        input = io.read()
+        if isInteger(input) then
+            width = tonumber(input)
+            break
+        end
+    end
+
+    local fuelLevel = turtle.getFuelLevel()
+    local requiredFuel = calculate_required_fuel_for_mining(length, width, height, doStripmining)
+    local belowRequiredFuel = fuelLevel < requiredFuel
+    if belowRequiredFuel == true then
+        print("Required Fuel: " .. requiredFuel)
+        print("Available Fuel: " .. fuelLevel)
+        while (belowRequiredFuel == true) do
+            print(
+                (requiredFuel - fuelLevel) ..
+                    " more fuel required to run. Please insert fuel in the first slot and press enter to refuel."
+            )
+            input = io.read()
+            turtle.select(1)
+            if turtle.getItemCount() > 0 then
+                while (turtle.getItemCount() > 0 and belowRequiredFuel == true) do
+                    if not turtle.refuel(1) then
+                        print("No valid fuel in the first slot!")
+                        break
+                    end
+                    fuelLevel = turtle.getFuelLevel()
+                    belowRequiredFuel = fuelLevel < requiredFuel
+                end
+            else
+                print("No fuel in the first slot!")
+            end
+        end
+    end
+    print("Ready to mine!")
+    local noMoreBlocks = false
+    -- Mining part here
+    for i = 1, length do
+        turtle.turnRight()
+        for i = 1, width do
+            noMoreBlocks = false
+            while not noMoreBlocks do
+                if turtle.detect() then
+                    turtle.dig()
+                    sleep(0.5)
+                    if not turtle.detect() then
+                        noMoreBlocks = true
+                    end
+                end
+            end
+        end
+    end
+end
+
+-- Program to dig a straigth tunnel
+local function command_tunnel()
+    do_mining(false)
+end
+
+-- Program to do some stripmining
+local function command_stripmine()
+    do_mining(true)
+end
+
+while true do
+    print("Please choose a command:")
+    local input = string.lower(io.read())
+
+    if input == "tunnel" then
+        command_tunnel()
+    elseif input == "help" then
+        command_help()
+    elseif input == "stripmine" then
+        command_stripmine()
+    elseif input == "exit" then
+        print("Exiting the program!")
+        break
+    else
+        print("Invalid command!")
+    end
+end
